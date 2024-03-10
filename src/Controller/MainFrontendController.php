48a0db7bdfe2149c10a456024b949b35774695d5
dawka300 <dawaw7@wp.pl>
2024-03-10 22:41:41 +0100

    testowy commit

--- src/Controller/MainFrontendController.php
+++ src/Controller/MainFrontendController.php
@@ -8,19 +8,19 @@ use Symfony\Component\Routing\Annotation\Route;
 
 class MainFrontendController extends AbstractController
 {
-    #[Route(path: '/', name: 'home', methods: ['GET'])]
+    #[Route(path: '/', name: 'homepage', methods: ['GET'])]
     public function index(): Response
     {
-        return $this->render('base.html.twig');
+        return $this->render('index.html.twig');
     }
 
     #[Route(path: '/contact', name: 'contact', methods: ['GET'])]
     public function contact(): Response
     {
-        return $this->render('base.html.twig');
+        return $this->render('contact.html.twig');
     }
 
-    #[Route(path: '/about', name: 'contact', methods: ['GET'])]
+    #[Route(path: '/about', name: 'about', methods: ['GET'])]
     public function about(): Response
     {
         return $this->render('base.html.twig');
